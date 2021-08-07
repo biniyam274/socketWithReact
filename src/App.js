c9a621cf3c8b432e1c960805c462a5592f501a3c
biniabera274 <biniabera274@gmail.com>
2021-08-07 16:12:32 +0300

    done!!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import Chat from "./components/chat/chat";
+import Process from "./components/process/process";
+import Home from "./components/home/home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
+import React ,{Fragment} from "react";
+import io from "socket.io-client";
 
-import './App.css';
+const socket = io.connect('/');
+
+function MainApp(props) {
+
+    console.log(props)
+    return (
+        <Fragment>
+                    <div className="container">
+                <div className="right">
+                    <Chat
+                        username={props.match.params.username}
+                        roomname={props.match.params.roomname}
+                        socket={socket}
+                    />
+                </div>
+                <div className="left">
+                    <Process />
+                </div>
+            </div>
+        </Fragment>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello World !</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/" exact>
+                        <Home socket={socket} />
+                    </Route>
+                    <Route path="/chat/:roomname/:username" component={MainApp} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import rootReducers from "./store/reducer/index";
+
 import './index.css';
 import App from './App';
 
+const store = createStore(rootReducers);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
